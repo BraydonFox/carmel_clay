183e685a2ff93a568f9cece293b0708b7fb8cec9
Braydon <foxden00@gmail.com>
2016-11-07 18:15:27 -0500

    Added styling for location info page.
    Removed styling for blog posts, and replaced it with style code for the
    location page.

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 	<div id="map" ng-init="init()"></div>
 	<div id="myLocationContainer">
 		<switch id="trackingSwitch" name="tracking" ng-model="isTrackingEnabled" on="On" off="Off" class="purple" ng-change="determineTracking"></switch>
-		<label>My Location: {{ isTrackingEnabled }}</label>
+		<label>Your Location</label>
 	</div>
-	<div ng-show="isLocationSelected">
-		
+	<div ng-show="isLocationSelected" id="locationPage">
+		<div id="locationInfo">
+			<h1>{{ Location.title }}</h1>
+			<p>{{ Location.description }}</p>
+		</div>
 	</div>
 <?php get_footer(); ?>
